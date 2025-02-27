7af18b3ae0f34910c31de8924e2bbd25ce2eda9f
Artem <artem6841@gmail.com>
2025-02-28 02:40:50 +0300

    added swiper for buttons

--- js/main.js
+++ js/main.js
@@ -1,14 +1,23 @@
 new Swiper('.destination__swiper', {
     slidesPerView: 3,
     slidesPerGroup: 1,
-    spaceBetween: 25,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: "#nextDestinationBtn",
+        prevEl: "#prevDestinationBtn",
+    },
 });
 
 new Swiper('.categories__swiper', {
     slidesPerView: 6,
     slidesPerGroup: 1,
     spaceBetween: 30,
-    loop: true
+    loop: true,
+    navigation: {
+        nextEl: "#nextBtn",
+        prevEl: "#prevBtn",
+    },
 });
 
 
